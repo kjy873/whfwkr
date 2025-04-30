6442b54aa6c687193ea3a7ec1fe77a19d0c1a52c
HSU0608 <tnsdnr0608@naver.com>
2025-04-30 21:27:19 +0900

    ai 캐릭터 생성
    ai 캐릭터를 생성하고 움직이는 모션을 구현할 예정

---
+++ Source/Tbd/NPC.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "NPC.generated.h"
+#include <BehaviorTree/BehaviorTree.h>
+
+UCLASS()
+class TBD_API ANPC : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ANPC();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	UBehaviorTree* GetBehaviorTree() const;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI", meta = (AllowPrivateAccess = "true"))
+	UBehaviorTree* Tree;
+};
