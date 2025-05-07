2c17aee2d26e59d0d74e1403d7e9253a1abe10a9
KJY <kjy873@tukorea.ac.kr>
2025-05-08 00:47:32 +0900

    랜드스케이프 변경
    랜드스케이프 높이 맵 설정을
    런타임 생성에서 고정 높이맵으로 변경
    빌드하면 런타임에 랜드스케이프 높이 맵을 설정할 수 없음

--- Source/Tbd/GM_Main.cpp
+++ Source/Tbd/GM_Main.cpp
@@ -12,24 +12,24 @@
 void AGM_Main::BeginPlay() {
 	Super::BeginPlay();
 
-	UWorld* World = GetWorld();
+	//UWorld* World = GetWorld();
 
-	ALandscape* Landscape = nullptr;
-	int32 WidthLV{ 0 }, HeightLV{ 0 };
-	TArray<int32> HeightMap;
-	HeightMap.SetNum(0);
-	if (World) {
-		UE_LOG(LogTemp, Warning, TEXT("found world. execute LandscapeHeightmapApplier"));
+	//ALandscape* Landscape = nullptr;
+	//int32 WidthLV{ 0 }, HeightLV{ 0 };
+	//TArray<int32> HeightMap;
+	//HeightMap.SetNum(0);
+	//if (World) {
+	//	UE_LOG(LogTemp, Warning, TEXT("found world. execute LandscapeHeightmapApplier"));
 
-		Landscape = UPCGFunctionLibrary::FindLandscape(World);
+	//	Landscape = UPCGFunctionLibrary::FindLandscape(World);
 
-		FVector CurrentScale = Landscape->GetActorScale3D();
-		Landscape->SetActorScale3D(FVector(CurrentScale.X, CurrentScale.Y, CurrentScale.Z));
+	//	FVector CurrentScale = Landscape->GetActorScale3D();
+	//	Landscape->SetActorScale3D(FVector(CurrentScale.X, CurrentScale.Y, CurrentScale.Z));
 
-		UPCGFunctionLibrary::GetLandscapeSize(Landscape, WidthLV, HeightLV);
-		HeightMap.SetNum(WidthLV * HeightLV);
-		UPCGFunctionLibrary::GenerateMHeightMap(HeightMap, WidthLV, HeightLV, 0.01f);
-		//UPCGFunctionLibrary::ErosionSimulator(HeightMap, WidthLV, HeightLV, 100);
-		UPCGFunctionLibrary::ApplyHeightMap(Landscape, HeightMap, WidthLV, HeightLV);
-	}
+	//	UPCGFunctionLibrary::GetLandscapeSize(Landscape, WidthLV, HeightLV);
+	//	HeightMap.SetNum(WidthLV * HeightLV);
+	//	UPCGFunctionLibrary::GenerateMHeightMap(HeightMap, WidthLV, HeightLV, 0.01f);
+	//	//UPCGFunctionLibrary::ErosionSimulator(HeightMap, WidthLV, HeightLV, 100);
+	//	UPCGFunctionLibrary::ApplyHeightMap(Landscape, HeightMap, WidthLV, HeightLV);
+	//}
 }
